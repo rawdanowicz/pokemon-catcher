5d28c7a834487ffba9e7fb523df1e7ba43e21c12
Marek Rawdanowicz <marek.rawdanowicz@outlook.com>
2021-01-09 15:01:03 +0100

    create pokemon component

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 @NgModule({
-  declarations: [StartScreenComponent, PokemonListComponent],
+  declarations: [StartScreenComponent, PokemonListComponent, PokemonComponent],
   imports: [SharedModule, ReactiveFormsModule],
 })
 export class GameModule {}
---
+++ src/app/game/pokemon/pokemon.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Pokemon } from 'src/app/shared/pokemon';
+
+@Component({
+  selector: 'app-pokemon',
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.scss'],
+})
+export class PokemonComponent implements OnInit {
+  @Input() pokemon!: Pokemon;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
