afea9914aca0dceac8acad41627896a214e9d8b3
Marek Rawdanowicz <marek.rawdanowicz@outlook.com>
2021-01-12 15:42:47 +0100

    add page-not-found

--- src/app/game/game-routing.module.ts
+++ src/app/game/game-routing.module.ts
@@ -3,10 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { PageNotFoundComponent } from '../shared/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: StartScreenComponent },
   { path: 'pokemons', component: PokemonListComponent },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [PageNotFoundComponent],
+  imports: [CommonModule, RouterModule],
   exports: [CommonModule],
 })
 export class SharedModule {}
